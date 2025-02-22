b8fb17fdd3372370b77e5ae284238349c8c9ecf3
Aman Soni <amansoni78695@gmail.com>
2025-02-22 21:25:33 +0530

    minor changes

--- app/components/homepage/contact/contact-without-captcha.jsx
+++ app/components/homepage/contact/contact-without-captcha.jsx
@@ -34,9 +34,9 @@ function ContactWithoutCaptcha() {
       setError({ ...error, required: false });
     }
 
-    const serviceID = 'service_froz1y6';
-    const templateID = 'template_qxobfxu';
-    const options = { publicKey: 'sUDSoetMH4joi5WQ0'};
+    const serviceID = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+    const templateID = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+    const options = { publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY};
     console.log(serviceID," ",templateID," ",options,"  ",input," ", "Dtaaaa")
 
     try {
